cc24cd17618506e3243487df7af2b4fa0b030a49
ecmmmeee <ec2437@gmail.com>
2020-11-22 21:25:24 -0600

    manually added wes branch in

---
+++ app/src/main/java/edu/utap/wanikani/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.utap.wanikani
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
