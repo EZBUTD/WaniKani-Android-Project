994010cecd5c4183747fe197b890a53a7ba1074c
Wesley Chu <wesleychu816@gmail.com>
2020-11-26 01:20:52 -0600

    Pushing changes. In the middle of working on lessons frag

--- app/src/main/java/edu/utap/wanikani/api/Repository.kt
+++ app/src/main/java/edu/utap/wanikani/api/Repository.kt
@@ -2,7 +2,7 @@ package edu.utap.wanikani.api
 
 class Repository(private val wanikaniApi: WanikaniApi) {
 
-    suspend fun fetchVocab(level : Int) : List<WanikaniSubjects>? {
-        return wanikaniApi.api_call(level).results
+    suspend fun fetchVocab(level : Int) : WanikaniSubjects? {
+        return wanikaniApi.api_call(level).data
     }
 }
