8fcf98981de680fc3b9dcaab9eab53950ff11509
Wesley Chu <wesleychu816@gmail.com>
2020-11-27 15:09:12 -0600

    quiz is working. Still some bugs with the layout. Improvements needed

--- app/src/main/java/edu/utap/wanikani/ui/Lesson.kt
+++ app/src/main/java/edu/utap/wanikani/ui/Lesson.kt
@@ -1,12 +1,16 @@
 package edu.utap.wanikani.ui
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.addCallback
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.replace
 import edu.utap.wanikani.MainViewModel
 import edu.utap.wanikani.R
 import kotlinx.android.synthetic.main.fragment_lesson.*
@@ -15,10 +19,13 @@ class Lesson : Fragment() {
 
     private val viewModel: MainViewModel by activityViewModels()
 
+    //Use myTypeId to tell if we want to display stuff for 0: radical, 1: kanji, 2: vocabulary
+    private var myTypeId : Int =0
+
     private var currentIdx : Int =0
     //fragment tabs for the radicals section
-    private var radicalTabsIdx : Int = 0
-    private val radicalTabs : List<String> = listOf("Name", "Examples")
+    private var tabCount : Int = 0
+    private var currentTabIdx : Int = 0
     private val radicalTabsTitles : List<String> = listOf("Name Mnemonic", "Kanji Examples")
 
     //Some hardcoded values just to see what my layout looks like.
@@ -29,6 +36,9 @@ class Lesson : Fragment() {
 
     //I think you would need a lateinit var like this? I'm not sure
     // private lateinit var lessonCharacters : List<String>
+
+    //private lateinit var lessonDone : MutableList<Boolean>          //Use this to check that User has gone through each tab in the lesson
+    private var lessonDone : MutableList<Boolean> = arrayListOf()          //Use this to check that User has gone through each tab in the lesson
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,18 +52,49 @@ class Lesson : Fragment() {
         meaningTV.text = debug_meaning[0]
     }
 
-    private fun initNameMnemonic_Examples(){
+    private fun initTabs(){
+        kanjiTab1TV.visibility = View.INVISIBLE
+        kanjiTab2TV.visibility = View.INVISIBLE
+        kanjiTab3TV.visibility = View.INVISIBLE
+        kanjiTab4TV.visibility = View.INVISIBLE
+
         tabTitleTV.text = radicalTabsTitles[0]
         textBlockTV.text = debug_nameMnemonic[0]
 
-        nameTV.setOnClickListener{
-            radicalTabsIdx=0
-            openTab(radicalTabsIdx)
+        radTab1TV.setOnClickListener{
+            currentTabIdx=0
+            openTab(currentTabIdx)
         }
 
-        examplesTV.setOnClickListener{
-            radicalTabsIdx=1
-            openTab(radicalTabsIdx)
+        radTab2TV.setOnClickListener{
+            currentTabIdx=1
+            openTab(currentTabIdx)
+        }
+    }
+
+    private fun initQuiz(){
+        quizBut.isClickable=false
+        quizBut.isEnabled=false
+
+        quizBut.setOnClickListener{
+            val quizFragment = ReviewQuiz.newInstance(myTypeId)
+            parentFragmentManager.beginTransaction()
+                    .replace(R.id.main_frame, quizFragment)
+                    .addToBackStack("backHome")
+                    .commit()
+        }
+    }
+    
+    private fun checkIfLessonDone() : Boolean {
+        if ( lessonDone.contains(false))
+            return false
+        else {
+            //XXX probably need to use the API to return some call back to the servers here
+            quizBut.isClickable=true
+            quizBut.isEnabled=true
+            quizBut.setBackgroundColor(Color.GREEN)
+            Toast.makeText(context, "Congratulations, you have finished the lesson and can now start the quiz", Toast.LENGTH_SHORT).show()
+            return true
         }
     }
 
@@ -70,25 +111,30 @@ class Lesson : Fragment() {
     private fun initLeftRightArrow(){
         leftArrowTV.setOnClickListener{
             decrementIdx()
-            radicalTabsIdx=0
-            openTab(radicalTabsIdx)
+            currentTabIdx=0
+            openTab(currentTabIdx)
 
             charTV.text = debug_characters[currentIdx]
             meaningTV.text = debug_meaning[currentIdx]
+            lessonDone[currentIdx*tabCount]=true
         }
 
         rightArrowTV.setOnClickListener{
-            if (radicalTabsIdx == radicalTabs.size-1) {
+            if (currentTabIdx == radicalTabsTitles.size-1) {
                 incrementIdx()
-                radicalTabsIdx=0
-                openTab(radicalTabsIdx)
+                currentTabIdx=0
+                openTab(currentTabIdx)
             } else {
-                radicalTabsIdx++
-                openTab(radicalTabsIdx)
+                currentTabIdx++
+                openTab(currentTabIdx)
             }
 
             charTV.text = debug_characters[currentIdx]
             meaningTV.text = debug_meaning[currentIdx]
+            val lessonDoneIdx = currentIdx*tabCount+currentTabIdx
+            lessonDone[lessonDoneIdx]=true
+            Log.d("XXXlessonDone", "$lessonDoneIdx is set to true")
+            checkIfLessonDone()
         }
     }
 
@@ -110,7 +156,6 @@ class Lesson : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_lesson, container, false)
 
-
         requireActivity().onBackPressedDispatcher.addCallback(this){
             parentFragmentManager.popBackStack()
         }
@@ -119,15 +164,40 @@ class Lesson : Fragment() {
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        if (this.arguments != null) {
+            myTypeId = this.requireArguments().getInt(typeIdKey)
+        }
+        // This tells us how many tabs there are based on whether this lesson is type: Radicals, Kanji, Vocabulary
+        when (myTypeId){
+            0->tabCount=2
+            1->tabCount=4
+            2->tabCount=3
+            else->tabCount=0
+        }
+
+        //I used a 2d array that's been flattened to 1d
+        for (i in 0 until debug_characters.size * tabCount) {
+            lessonDone.add(false)
+            Log.d("XXXlessonDone", "$i is set to false")
+        }
+        lessonDone[0] = true
+
+        Log.d("XXXtypeid", "$myTypeId")
         initCharacters()
         initMeaning()
-        initNameMnemonic_Examples()
+        initTabs()
+        initQuiz()
         initLeftRightArrow()
     }
 
     companion object {
-        fun newInstance() : Lesson {
-            return Lesson()
+        const val typeIdKey = "typeIdKey"
+        fun newInstance(typeId: Int) : Lesson {
+            val b = Bundle()
+            b.putInt(typeIdKey, typeId)
+            val frag = Lesson()
+            frag.arguments = b
+            return frag
         }
     }
 }
---
+++ app/src/main/java/edu/utap/wanikani/ui/ReviewQuiz.kt
@@ -0,0 +1,138 @@
+package edu.utap.wanikani.ui
+
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.fragment.app.activityViewModels
+import edu.utap.wanikani.MainViewModel
+import edu.utap.wanikani.R
+import kotlinx.android.synthetic.main.fragment_review_quiz.*
+
+class ReviewQuiz : Fragment() {
+
+    private val viewModel: MainViewModel by activityViewModels()
+
+    //Use myTypeId to tell if we want to display stuff for 0: radical, 1: kanji, 2: vocabulary
+    private var myTypeId : Int =0
+
+    private var currentIdx : Int =0
+    private var tries : Int = 0
+    //fragment tabs for the radicals section
+
+    //Some hardcoded values just to see what my layout looks like.
+    private val debug_characters : List<String> = listOf("一", "ハ")
+    private val debug_answers : List<String> = listOf("Ground", "Fins")
+
+    //private lateinit var questionDone : MutableList<Boolean>
+    private var questionDone : MutableList<Boolean> = arrayListOf()
+
+    private fun initCharacters(){
+        charTV.text = debug_characters[0]
+    }
+
+    private fun initTitle(){
+        nameTypeTV.text = "Radical Name"
+    }
+
+    private fun initAnswerCheck(){
+        answerArrow.setOnClickListener{
+            checkAnswer()
+        }
+    }
+
+    private fun checkAnswer() {
+        if (responseET.text.toString().toLowerCase() == debug_answers[currentIdx].toLowerCase()) {
+            answerLay.setBackgroundColor(Color.GREEN)
+            questionDone[currentIdx] = true
+            Thread.sleep(500)
+            Log.d("XXXcheckanswer", "correct answer")
+
+            if (lessonFinished()){
+                parentFragmentManager.popBackStack()
+                parentFragmentManager.popBackStack()
+            } else
+                nextQuestion()
+
+        } else {
+            if (tries == 1) {
+                nextQuestion()
+            } else {
+                answerLay.setBackgroundColor(Color.RED)
+                Thread.sleep(500)
+                tries++
+            }
+        }
+    }
+
+    private fun nextQuestion() {
+        tries = 0
+
+        var nextIdx = questionDone.subList(currentIdx+1, questionDone.size).indexOf(false)
+        Log.d("XXXnextQuestion0", "$nextIdx is the next idx")
+        if(nextIdx == -1) {
+            nextIdx = questionDone.indexOf(false)
+        } else {
+            //Accounts for the offset introduced by taking the sublist
+            nextIdx+= currentIdx+1
+        }
+
+        Log.d("XXXnextQuestion1", "$nextIdx is the next idx")
+
+        currentIdx = nextIdx
+        responseET.setBackgroundColor(Color.WHITE)
+        charTV.text = debug_characters[currentIdx]
+        responseET.text.clear()
+    }
+
+    private fun lessonFinished() :Boolean {
+        return !questionDone.contains(false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_review_quiz, container, false)
+
+        requireActivity().onBackPressedDispatcher.addCallback(this){
+            parentFragmentManager.popBackStack()
+        }
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        if (this.arguments != null) {
+            myTypeId = this.requireArguments().getInt(Lesson.typeIdKey)
+        }
+        Log.d("XXXtypeid", "$myTypeId")
+        for (i in debug_answers.indices){
+            //questionDone[i] = false
+            questionDone.add(false)
+            Log.d("XXXquizDone", "$i is set to false")
+        }
+        initCharacters()
+        initTitle()
+        initAnswerCheck()
+    }
+
+    companion object {
+        const val typeIdKey = "typeIdKey"
+        fun newInstance(typeId: Int) : ReviewQuiz {
+            val b = Bundle()
+            b.putInt(typeIdKey, typeId)
+            val frag = ReviewQuiz()
+            frag.arguments = b
+            return frag
+        }
+    }
+}
